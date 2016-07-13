f99eb9d103749844fea00e084da6e8453f41051e
archco <archcoster@gmail.com>
2016-07-13 17:04:20 +0900

    Fixed test

--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,10 @@
 <?php 
 require_once __DIR__.'/../vendor/autoload.php';
+include_once 'db_config.php';
 
 use cosmos\database\MysqliDatabase;
 
-$mysqli = new mysqli('localhost', 'my_user', 'my_password', 'my_db');
+$mysqli = new mysqli($host, $user, $password, $db_name);
 $mdb = new MysqliDatabase($mysqli);
 
 echo $mdb->stat();
